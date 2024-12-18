6627bbfa008b90692ac78e4ec77e12f849f72b9c
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-12-18 21:42:49 +0530

    flask app initialised

---
+++ server.py
@@ -0,0 +1,19 @@
+from app import *
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to AI Resune Parser!"})
+
+@app.route('/api/test', methods=['GET'])
+def test():
+    return jsonify({"message": "API is working"})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
