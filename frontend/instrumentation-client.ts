fd5f067909d77ca36357935b729a85e522c0ce2d
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-10-22 17:41:51 +0530

    chore: removed debug logs

--- frontend/instrumentation-client.ts
+++ frontend/instrumentation-client.ts
@@ -5,8 +5,6 @@ if (typeof window !== 'undefined') {
   const apiHost = '/ph'
   const uiHost = "https://eu.posthog.com"
 
-  console.log('[PH] init', { hasKey: !!key, apiHost, uiHost })
-
   posthog.init(key as string, {
     api_host: apiHost,
     ui_host: uiHost,
